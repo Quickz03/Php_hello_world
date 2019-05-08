1cee44d1e36d0ef4035f98463e5aa646d6efdc22
Brian Zilka <Bzilks@gmail.com>
2019-05-08 09:06:13 -0500

    hello world up on screen and database connected

--- helloWorld.php
+++ helloWorld.php
@@ -3,6 +3,13 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
  </body>
 </html>
+
+
